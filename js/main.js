1ed186e787153b25ebdea3679ecfa2fad374142e
Dan1521 <danilsokolov779@gmail.com>
2022-05-25 16:53:25 +0300

    добавил меню

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
   const cardBtns = document.querySelectorAll('[data-pop]');
   const modals = document.querySelectorAll('.modal');
-
+  const supplerBtn = document.querySelector('.suppler');
+  const menu = document.querySelector('.menu');
+  const menuContainer = menu.querySelector('.menu__container')
+  const menuHeight = menuContainer.scrollHeight;
   cardBtns.forEach((element) => {
     element.addEventListener('click', (event) => {
       event.preventDefault();
@@ -26,6 +29,18 @@ document.addEventListener('DOMContentLoaded', () => {
         modal.classList.remove('modal--open')
       }
     })
+  })
+
+  supplerBtn.addEventListener('click', (event) => {
+    event.currentTarget.classList.toggle('suppler--active');
+
+    if (!menu.classList.contains('menu--open')) {
+      menu.classList.add('menu--open');
+      menuContainer.style.maxHeight = menuHeight + 'px';
+    } else {
+      menu.classList.remove('menu--open');
+      menuContainer.style.maxHeight = 0;
+    }
   })
 
 
