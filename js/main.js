63e7fa668e8d7787f93c2e9ff55895215f1b2591
Dan1521 <danilsokolov779@gmail.com>
2022-05-25 18:58:32 +0300

    работа над меню

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
   const cardBtns = document.querySelectorAll('[data-pop]');
   const modals = document.querySelectorAll('.modal');
-  const supplerBtn = document.querySelector('.suppler');
-  const menu = document.querySelector('.menu');
-  const menuContainer = menu.querySelector('.menu__container')
-  const menuHeight = menuContainer.scrollHeight;
+  const menuBtn = document.querySelectorAll('[data-menu-btn]');
+  const menus = document.querySelectorAll('[data-menu]');
+
   cardBtns.forEach((element) => {
     element.addEventListener('click', (event) => {
       event.preventDefault();
@@ -31,17 +30,60 @@ document.addEventListener('DOMContentLoaded', () => {
     })
   })
 
-  supplerBtn.addEventListener('click', (event) => {
-    event.currentTarget.classList.toggle('suppler--active');
+  menuBtn.forEach((elem) => {
+    elem.addEventListener('click', (event) => {
+      let self = event.currentTarget;
+      let selfClass = self.dataset.menuBtn
 
-    if (!menu.classList.contains('menu--open')) {
-      menu.classList.add('menu--open');
-      menuContainer.style.maxHeight = menuHeight + 'px';
-    } else {
-      menu.classList.remove('menu--open');
-      menuContainer.style.maxHeight = 0;
-    }
+      menus.forEach((menu) => {
+        let menuClass = menu.dataset.menu
+        let menuHeight = menu.scrollHeight;
+
+        if (selfClass == menuClass) {
+          if (!menu.classList.contains('menu--open')) {
+            menuBtn.forEach((elem) => {
+              elem.classList.remove('is-active');
+            })
+            menus.forEach((elem) => {
+              elem.classList.remove('menu--open');
+            })
+            menu.classList.add('menu--open');
+            self.classList.add('is-active');
+            menu.style.maxHeight = menuHeight + 'px';
+          } else {
+            menu.classList.remove('menu--open');
+            self.classList.remove('is-active');
+            menu.style.maxHeight = 0;
+          } 
+        }
+      });
+    })
+    
   })
+
+  // burger.addEventListener('click', (event) => {
+  //   if (!burger.classList.contains('burger-active')) {
+  //     menuNav.classList.add('menu--open');
+  //     burger.classList.add('burger-active')
+  //     menuNav.style.maxHeight = menuNavHeight + 'px';
+  //   } else {
+  //     menuNav.classList.remove('menu--open');
+  //     burger.classList.remove('burger-active')
+  //     menuNav.style.maxHeight = 0;
+  //   }
+  // })
+
+  // suppler.addEventListener('click', (event) => {
+  //   event.currentTarget.classList.toggle('suppler--active');
+
+  //   if (!menuCat.classList.contains('menu--open')) {
+  //     menuCat.classList.add('menu--open');
+  //     menuCat.style.maxHeight = menuCatHeight + 'px';
+  //   } else {
+  //     menu.classList.remove('menu--open');
+  //     menuCat.style.maxHeight = 0;
+  //   }
+  // })
 
 
 
